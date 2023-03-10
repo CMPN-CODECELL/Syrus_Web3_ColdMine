08d9f910fce4778501b6b42c58cbe72cbe704cc0
grv-sngh <gauravsingh3050@gmail.com>
2023-03-10 10:40:11 +0530

    medical record

---
+++ src/pages/medical_record.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Record() {
+    return (
+        <>
+
+        
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+        </>
+    );
+}
